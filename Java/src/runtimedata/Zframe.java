ab6e1ebe9314bd7cf4ce9f934e6158f5374b39f0
zachaxy <zachaxy@163.com>
2017-05-04 21:25:15 +0800

    添加线程私有数据区的栈帧类

--- Java/src/runtimedata/Zframe.java
+++ Java/src/runtimedata/Zframe.java
@@ -3,12 +3,13 @@ package runtimedata;
 /**
  * Author: zhangxin
  * Time: 2017/5/4 0004.
- * Desc:
+ * Desc:栈帧,执行方法所需的局部变量表大小和操作数栈深度是由编译器预先计算好的，存储在class文件method_info结构的Code属性中
+ * 详细实现参考 {@link classfile.CodeAttribute}
  */
 public class Zframe {
-    Zframe lower;       //当前帧的 前一帧的引用;
-    LocalVars localVars;
-    OperandStack operandStack;
+    Zframe lower;       //当前帧的 前一帧的引用;相当于单向链表的前一个指针
+    LocalVars localVars;    //局部变量表的引用;
+    OperandStack operandStack;  //操作数栈的引用;
 
     // TODO: 2017/5/4 0004
     public Zframe(int maxLocals, int maxStack) {
