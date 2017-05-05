bf37c73943ab07ee7819b92d5fba58d267141d1e
zachaxy <zachaxy@163.com>
2017-05-05 23:37:12 +0800

    添加ifacmp指令

---
+++ Java/src/instructions/comparisons/ifacmp/IF_ACMPNE.java
@@ -0,0 +1,19 @@
+package instructions.comparisons.ifacmp;
+
+import instructions.base.BranchInstruction;
+import instructions.base.BranchLogic;
+import runtimedata.Zframe;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class IF_ACMPNE extends BranchInstruction {
+    @Override
+    public void execute(Zframe frame) {
+        if (!IfAcmp._acmp(frame)) {
+            BranchLogic.branch(frame, offset);
+        }
+    }
+}
---
+++ Java/src/instructions/comparisons/ifacmp/IfAcmp.java
@@ -0,0 +1,19 @@
+package instructions.comparisons.ifacmp;
+
+import runtimedata.OperandStack;
+import runtimedata.Zframe;
+import runtimedata.Zobject;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class IfAcmp {
+    public static boolean _acmp(Zframe frame) {
+        OperandStack stack = frame.getOperandStack();
+        Zobject ref2 = stack.popRef();
+        Zobject ref1 = stack.popRef();
+        return ref1 == ref2;
+    }
+}
