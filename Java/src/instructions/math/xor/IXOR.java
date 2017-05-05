d2e682680985dcaff0debc73606d897f87fc363f
zachaxy <zachaxy@163.com>
2017-05-05 21:20:10 +0800

    添加按位异或指令

---
+++ Java/src/instructions/math/xor/IXOR.java
@@ -0,0 +1,21 @@
+package instructions.math.xor;
+
+import instructions.base.NoOperandsInstruction;
+import runtimedata.OperandStack;
+import runtimedata.Zframe;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class IXOR extends NoOperandsInstruction {
+    @Override
+    public void execute(Zframe frame) {
+        OperandStack stack = frame.getOperandStack();
+        int val2 = stack.popInt();
+        int val1 = stack.popInt();
+        int res = val1 ^ val2;
+        stack.pushInt(res);
+    }
+}
