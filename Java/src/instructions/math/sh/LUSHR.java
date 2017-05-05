b2ae667e02a350f8edb1d97fde99c8d56e67cd5b
zachaxy <zachaxy@163.com>
2017-05-05 21:12:31 +0800

    修改对于long的位移指令,位移数位0x3f(6位)

--- Java/src/instructions/math/sh/LUSHR.java
+++ Java/src/instructions/math/sh/LUSHR.java
@@ -15,7 +15,7 @@ public class LUSHR extends NoOperandsInstruction {
         OperandStack stack = frame.getOperandStack();
         int val2 = stack.popInt();  //要移动多少bit
         long val1 = stack.popLong();  //要进行位移操作的变量
-        int s = val2 & 0x1f; //int变量只有32位，所以只取val2的后5个比特就足够表示位移位数了,位移操作符右侧必须是无符号整数，所以需要对val2进行类型转换
+        int s = val2 & 0x3f; //int变量只有32位，所以只取val2的后5个比特就足够表示位移位数了,位移操作符右侧必须是无符号整数，所以需要对val2进行类型转换
 
         //但是Java中对于大数左移超出后,也会变成负数,所以这里不做额外处理了
         long res = val1 >>> s;
