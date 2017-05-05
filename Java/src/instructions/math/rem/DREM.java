f61dae9a9d8c48ca199cf74b4ec6927c48a314bb
zachaxy <zachaxy@163.com>
2017-05-05 19:50:34 +0800

    添加取余指令

---
+++ Java/src/instructions/math/rem/DREM.java
@@ -0,0 +1,22 @@
+package instructions.math.rem;
+
+import instructions.base.NoOperandsInstruction;
+import runtimedata.OperandStack;
+import runtimedata.Zframe;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class DREM extends NoOperandsInstruction {
+    @Override
+    public void execute(Zframe frame) {
+        OperandStack stack = frame.getOperandStack();
+        double val1 = stack.popDouble();
+        double val2 = stack.popDouble();
+        double res = val1 % val2;
+        stack.pushDouble(res);
+    }
+}
+
