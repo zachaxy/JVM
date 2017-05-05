dbd1e344a9bdf4f820b945e176331257ee993e9c
zachaxy <zachaxy@163.com>
2017-05-05 15:08:28 +0800

    添加float存储指令

---
+++ Java/src/instructions/stores/storefloat/FSTORE_1.java
@@ -0,0 +1,17 @@
+package instructions.stores.storefloat;
+
+import instructions.base.NoOperandsInstruction;
+import instructions.stores.Store;
+import runtimedata.Zframe;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class FSTORE_1 extends NoOperandsInstruction {
+    @Override
+    public void execute(Zframe frame) {
+        Store.fstore(frame,1);
+    }
+}
