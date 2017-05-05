5efbe092b32fd84346c7ebacf7dead2a9df49f4f
zachaxy <zachaxy@163.com>
2017-05-05 19:32:45 +0800

    添加乘法指令

---
+++ Java/src/instructions/math/mul/FMUL.java
@@ -0,0 +1,21 @@
+package instructions.math.mul;
+
+import instructions.base.NoOperandsInstruction;
+import runtimedata.OperandStack;
+import runtimedata.Zframe;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class FMUL extends NoOperandsInstruction {
+    @Override
+    public void execute(Zframe frame) {
+        OperandStack stack = frame.getOperandStack();
+        float val1 = stack.popFloat();
+        float val2 = stack.popFloat();
+        float res = val1 * val2;
+        stack.pushFloat(res);
+    }
+}
---
+++ Java/src/instructions/math/mul/LMUL.java
@@ -0,0 +1,21 @@
+package instructions.math.mul;
+
+import instructions.base.NoOperandsInstruction;
+import runtimedata.OperandStack;
+import runtimedata.Zframe;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class LMUL extends NoOperandsInstruction {
+    @Override
+    public void execute(Zframe frame) {
+        OperandStack stack = frame.getOperandStack();
+        long val1 = stack.popLong();
+        long val2 = stack.popLong();
+        long res = val1 * val2;
+        stack.pushLong(res);
+    }
+}
