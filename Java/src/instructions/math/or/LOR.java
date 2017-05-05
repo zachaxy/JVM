b06bf3e237375826137dcefcad8f0b987b98bae3
zachaxy <zachaxy@163.com>
2017-05-05 21:18:54 +0800

    添加按位或指令

---
+++ Java/src/instructions/math/or/LOR.java
@@ -0,0 +1,21 @@
+package instructions.math.or;
+
+import instructions.base.NoOperandsInstruction;
+import runtimedata.OperandStack;
+import runtimedata.Zframe;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class LOR extends NoOperandsInstruction {
+    @Override
+    public void execute(Zframe frame) {
+        OperandStack stack = frame.getOperandStack();
+        long val2 = stack.popLong();
+        long val1 = stack.popLong();
+        long res = val1 | val2;
+        stack.pushLong(res);
+    }
+}
