85f4c65b099d07f3ed3beed0c99c407cef84e635
zachaxy <zachaxy@163.com>
2017-05-05 23:04:23 +0800

    添加fcmp指令

---
+++ Java/src/instructions/comparisons/fcmp/FCMPL.java
@@ -0,0 +1,16 @@
+package instructions.comparisons.fcmp;
+
+import instructions.base.NoOperandsInstruction;
+import runtimedata.Zframe;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class FCMPL extends NoOperandsInstruction {
+    @Override
+    public void execute(Zframe frame) {
+        FCMP._fcmp(frame, false);
+    }
+}
