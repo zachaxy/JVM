13f974dcb69b4d257c3431986540b2bf2abbc954
zachaxy <zachaxy@163.com>
2017-05-05 11:36:44 +0800

    添加加载指令的编码框架

---
+++ Java/src/instructions/loads/Load.java
@@ -0,0 +1,37 @@
+package instructions.loads;
+
+import runtimedata.Zframe;
+import runtimedata.Zobject;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc: 工具类,为不同的数据类型提供不同的加载机制;
+ * 总体原则是:先从本地变量表中取出变量,然后将该变量压入到操作数栈中;
+ */
+public class Load {
+    public static void aload(Zframe frame, int index) {
+        Zobject ref = frame.getLocalVars().getRef(index);
+        frame.getOperandStack().pushRef(ref);
+    }
+
+    public  static void dload(Zframe frame, int index) {
+        double val = frame.getLocalVars().getDouble(index);
+        frame.getOperandStack().pushDouble(val);
+    }
+
+    public  static void fload(Zframe frame, int index) {
+        float val = frame.getLocalVars().getFloat(index);
+        frame.getOperandStack().pushFloat(val);
+    }
+
+    public  static void iload(Zframe frame, int index) {
+        int val = frame.getLocalVars().getInt(index);
+        frame.getOperandStack().pushInt(val);
+    }
+
+    public static void lload(Zframe frame, int index) {
+        long val = frame.getLocalVars().getLong(index);
+        frame.getOperandStack().pushLong(val);
+    }
+}
