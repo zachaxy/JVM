42358299f965e691305ab2d5781c833708411dd9
zachaxy <zachaxy@163.com>
2017-05-06 10:48:23 +0800

    添加ifnull/ifnonnull指令

---
+++ Java/src/instructions/extended/IFNONNULL.java
@@ -0,0 +1,21 @@
+package instructions.extended;
+
+import instructions.base.BranchInstruction;
+import instructions.base.BranchLogic;
+import runtimedata.Zframe;
+import runtimedata.Zobject;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/6 0006.
+ * Desc:
+ */
+public class IFNONNULL extends BranchInstruction {
+    @Override
+    public void execute(Zframe frame) {
+        Zobject ref = frame.getOperandStack().popRef();
+        if (ref != null) {
+            BranchLogic.branch(frame, offset);
+        }
+    }
+}
