bf6b50cc4fff1f35ec3e67c5b879f8561be629b7
zachaxy <zachaxy@163.com>
2017-07-27 00:01:27 +0800

    添加instance_of指令,线程池常量的转换尚未实现

---
+++ Java/src/instructions/references/INSTANCE_OF.java
@@ -0,0 +1,35 @@
+package instructions.references;
+
+import instructions.base.Index16Instruction;
+import runtimedata.OperandStack;
+import runtimedata.Zframe;
+import runtimedata.heap.ClassRef;
+import runtimedata.heap.Zclass;
+import runtimedata.heap.ZconstantPool;
+import runtimedata.heap.Zobject;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/7/26.
+ * Desc:
+ */
+public class INSTANCE_OF extends Index16Instruction {
+    @Override
+    public void execute(Zframe frame) {
+        OperandStack stack = frame.getOperandStack();
+        Zobject ref = stack.popRef();
+        if (ref == null) {
+            stack.pushInt(0);
+            return;
+        }
+
+        ZconstantPool cp = frame.getMethod().getClassMember().getClazz().getConstantPool();
+        ClassRef classRef = null;// cp.getConstant(this.index);
+        Zclass clazz = classRef.resolvedClass();
+        if (ref.isInstanceOf(clazz)) {
+            stack.pushInt(1);
+        } else {
+            stack.pushInt(0);
+        }
+    }
+}
