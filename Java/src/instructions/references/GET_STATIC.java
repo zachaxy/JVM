802927d1edabf3e5ed3b51740efd0d0d8257080d
zachaxy <zachaxy@163.com>
2017-07-26 23:57:34 +0800

    添加get_static指令,线程池常量的转换尚未实现

---
+++ Java/src/instructions/references/GET_STATIC.java
@@ -0,0 +1,66 @@
+package instructions.references;
+
+import instructions.base.Index16Instruction;
+import runtimedata.OperandStack;
+import runtimedata.Slots;
+import runtimedata.Zframe;
+import runtimedata.heap.FieldRef;
+import runtimedata.heap.Zclass;
+import runtimedata.heap.ZconstantPool;
+import runtimedata.heap.Zfield;
+import runtimedata.heap.Zmethod;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/7/26.
+ * Desc:
+ */
+public class GET_STATIC extends Index16Instruction {
+    @Override
+    public void execute(Zframe frame) {
+        Zmethod currentMethod = frame.getMethod();
+        Zclass currentClass = currentMethod.getClassMember().getClazz();
+        ZconstantPool cp = currentClass.getConstantPool();
+
+        // TODO: 2017/7/26 常量池的转换尚未实现;
+        FieldRef fieldRef = null;// cp.getConstant(this.index);
+        Zfield field = fieldRef.resolvedField();
+        Zclass clazz = field.getClassMember().getClazz();
+        // todo: init class
+
+        if (!field.getClassMember().isStatic()) {
+            throw new RuntimeException("java.lang.IncompatibleClassChangeError");
+        }
+
+        if (field.getClassMember().isFinal()) {
+            if (currentClass != clazz || "<clinit>".equals(currentMethod.getClassMember().getName())) {
+                throw new RuntimeException("java.lang.IllegalAccessError");
+            }
+        }
+
+        String descriptor = field.getClassMember().getDescriptor();
+        int slotId = field.getSlotId();
+        Slots slots = clazz.getStaticVars();
+        OperandStack stack = frame.getOperandStack();
+
+        switch (descriptor.charAt(0)) {
+            case 'Z':
+            case 'B':
+            case 'C':
+            case 'S':
+            case 'I':
+                stack.pushInt(slots.getInt(slotId));
+            case 'F':
+                stack.pushFloat(slots.getFloat(slotId));
+            case 'J':
+                stack.pushLong(slots.getLong(slotId));
+            case 'D':
+                stack.pushDouble(slots.getDouble(slotId));
+            case 'L':
+            case '[':
+                stack.pushRef(slots.getRef(slotId));
+            default:
+                // todo
+        }
+    }
+}
