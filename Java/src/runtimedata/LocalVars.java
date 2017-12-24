c9e5ec200299587742b13167d17aaeeeb125dc65
zachaxy <zachaxy@163.com>
2017-12-24 20:18:29 +0800

    optimize LocalVars

--- Java/src/runtimedata/LocalVars.java
+++ Java/src/runtimedata/LocalVars.java
@@ -1,7 +1,5 @@
 package runtimedata;
 
-import runtimedata.heap.Zobject;
-
 /**
  * Author: zhangxin
  * Time: 2017/5/4 0004.
@@ -9,72 +7,8 @@ import runtimedata.heap.Zobject;
  * 根据Java虚拟机规范，这个数组的每个元素至少可以容纳一个int或引用值，两个连续的元素可以容纳一个long或double值。
  * 注:这里并没有真的对boolean、byte、short和char类型定义存取方法，因为这些类型的值都是转换成int值类来处理的(4K对齐)
  */
-public class LocalVars {
-    private Slot[] localVars; //局部变量表,JVM规定其按照索引访问,所以将其设置为数组
-
-    public LocalVars(int maxLocals) {
-        if (maxLocals > 0) {
-            localVars = new Slot[maxLocals];
-        } else {
-            throw new NullPointerException("maxLocals<0");
-        }
-    }
-
-    //提供了对int,float,long,double,引用的存取,这里要注意的是long和double是占用8字节的,所以使用了局部变量表中的两个槽位分别存储前四字节和后四字节
-    public void setInt(int index, int val) {
-        Slot slot = new Slot();
-        slot.num = val;
-        localVars[index] = slot;
-    }
-
-    public int getInt(int index) {
-        return localVars[index].num;
-    }
-
-    public void setFloat(int index, float val) {
-        Slot slot = new Slot();
-        slot.num = Float.floatToIntBits(val);
-        localVars[index] = slot;
-    }
-
-    public float getFloat(int index) {
-        return Float.intBitsToFloat(localVars[index].num);
-    }
-
-    public void setLong(int index, long val) {
-        //先存低32位
-        Slot slot1 = new Slot();
-        slot1.num = (int) (val);
-        localVars[index] = slot1;
-        //再存高32位
-        Slot slot2 = new Slot();
-        slot2.num = (int) (val >> 32);
-        localVars[index + 1] = slot2;
-    }
-
-    public long getLong(int index) {
-        int low = localVars[index].num;
-        long high = localVars[index + 1].num;
-        return ((high & 0x000000ffffffffL) << 32) | (low & 0x00000000ffffffffL);
-    }
-
-    public void setDouble(int index, double val) {
-        long bits = Double.doubleToLongBits(val);
-        setLong(index, bits);
-    }
-
-    public double getDouble(int index) {
-        long bits = getLong(index);
-        return Double.longBitsToDouble(bits);
-    }
-
-    public void setRef(int index, Zobject ref) {
-        Slot slot = new Slot();
-        slot.ref = ref;
-        localVars[index] = slot;
-    }
-
-    public Zobject getRef(int index) {
-        return localVars[index].ref;
+public class LocalVars extends Slots {
+    public LocalVars(int size) {
+        super(size);
     }
 }
