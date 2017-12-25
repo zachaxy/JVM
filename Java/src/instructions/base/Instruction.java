2f05331f73179298f5e29b763cc09c9712052db9
zachaxy <zachaxy@163.com>
2017-12-25 10:11:25 +0800

    add base Instruction annotate

--- Java/src/instructions/base/Instruction.java
+++ Java/src/instructions/base/Instruction.java
@@ -5,7 +5,7 @@ import runtimedata.Zframe;
 /**
  * Author: zhangxin
  * Time: 2017/5/5 0005.
- * Desc:
+ * Desc: 每个指令都会实现该接口，所有的指令逻辑都是先从字节码数组中取数据，然后执行各自的逻辑
  */
 public interface Instruction {
     //从字节码中提取操作数
