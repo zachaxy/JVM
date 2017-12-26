c056586a5aa9df53612ad517cd4ed3d3113206d4
zachaxy <zachaxy@163.com>
2017-12-26 15:22:20 +0800

    update instruction instance_of

--- Java/src/instructions/references/INSTANCE_OF.java
+++ Java/src/instructions/references/INSTANCE_OF.java
@@ -4,29 +4,31 @@ import instructions.base.Index16Instruction;
 import runtimedata.OperandStack;
 import runtimedata.Zframe;
 import runtimedata.heap.ClassRef;
-import runtimedata.heap.Zclass;
 import runtimedata.heap.RuntimeConstantPool;
+import runtimedata.heap.Zclass;
 import runtimedata.heap.Zobject;
 
 /**
  * Author: zhangxin
  * Time: 2017/7/26.
- * Desc:
+ * Desc: java 中的 instanceof 关键字，会被解析成 INSTANCE_OF 指令 eg:(obj instanceof Object)
+ * 将判断的结果写入操作数栈
  */
 public class INSTANCE_OF extends Index16Instruction {
     @Override
     public void execute(Zframe frame) {
         OperandStack stack = frame.getOperandStack();
-        Zobject ref = stack.popRef();
-        if (ref == null) {
+        Zobject obj = stack.popRef();
+        //如果 obj 为 null，对任何 class 类型的判断都是 false；
+        if (obj == null) {
             stack.pushInt(0);
             return;
         }
 
-        RuntimeConstantPool cp = frame.getMethod().getClazz().getRuntimeConstantPool();
-        ClassRef classRef = null;// cp.getConstant(this.index);
+        RuntimeConstantPool runtimeConstantPool = frame.getMethod().getClazz().getRuntimeConstantPool();
+        ClassRef classRef = (ClassRef) runtimeConstantPool.getRuntimeConstant(index).getValue();
         Zclass clazz = classRef.resolvedClass();
-        if (ref.isInstanceOf(clazz)) {
+        if (obj.isInstanceOf(clazz)) {
             stack.pushInt(1);
         } else {
             stack.pushInt(0);
