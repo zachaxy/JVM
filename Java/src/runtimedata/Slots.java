243df83567c47762cac895a99c0d9092ec2e2ac3
zachaxy <zachaxy@163.com>
2017-12-27 17:19:45 +0800

    fix bug for loading class during prepare with value 0

--- Java/src/runtimedata/Slots.java
+++ Java/src/runtimedata/Slots.java
@@ -8,16 +8,20 @@ import runtimedata.heap.Zobject;
  * Desc:
  */
 public class Slots {
-    Slot[] slots;
-    public Slots(int size){
+    private Slot[] slots;
+
+    public Slots(int size) {
         slots = new Slot[size];
+        for (int i = 0; i < size; i++) {
+            slots[i] = new Slot();
+        }
     }
 
     //提供了对int,float,long,double,引用的存取,这里要注意的是long和double是占用8字节的,所以使用了局部变量表中的两个槽位分别存储前四字节和后四字节
     public void setInt(int index, int val) {
-        Slot slot = new Slot();
-        slot.num = val;
-        slots[index] = slot;
+//        Slot slot = new Slot();
+//        slot.num = val;
+        slots[index].num = val;
     }
 
     public int getInt(int index) {
@@ -25,9 +29,9 @@ public class Slots {
     }
 
     public void setFloat(int index, float val) {
-        Slot slot = new Slot();
-        slot.num = Float.floatToIntBits(val);
-        slots[index] = slot;
+//        Slot slot = new Slot();
+//        slot.num = Float.floatToIntBits(val);
+        slots[index].num = Float.floatToIntBits(val);
     }
 
     public float getFloat(int index) {
@@ -36,13 +40,13 @@ public class Slots {
 
     public void setLong(int index, long val) {
         //先存低32位
-        Slot slot1 = new Slot();
-        slot1.num = (int) (val);
-        slots[index] = slot1;
+//        Slot slot1 = new Slot();
+//        slot1.num = (int) (val);
+        slots[index].num = (int) (val);
         //再存高32位
-        Slot slot2 = new Slot();
-        slot2.num = (int) (val >> 32);
-        slots[index + 1] = slot2;
+//        Slot slot2 = new Slot();
+//        slot2.num = (int) (val >> 32);
+        slots[index + 1].num = (int) (val >> 32);
     }
 
     public long getLong(int index) {
@@ -62,9 +66,9 @@ public class Slots {
     }
 
     public void setRef(int index, Zobject ref) {
-        Slot slot = new Slot();
-        slot.ref = ref;
-        slots[index] = slot;
+//        Slot slot = new Slot();
+//        slot.ref = ref;
+        slots[index].ref = ref;
     }
 
     public Zobject getRef(int index) {
