1064c6d937482cdb981189ced575a9ccc262d163
zachaxy <zachaxy@163.com>
2017-12-27 11:47:04 +0800

    fix bug in check_cast

--- Java/src/instructions/references/CHECK_CAST.java
+++ Java/src/instructions/references/CHECK_CAST.java
@@ -13,12 +13,14 @@ import runtimedata.heap.Zobject;
  * @date 17/12/26
  * desc：类型转换,该指令和instanceof指令的区别在于,instanceof判断后将结果压入操作数栈,而cast直接抛出异常
  * String str = (String)obj
+ * NOTE:checkcast 指令，在pop到引用 obj 之后，又将 obj push 到栈中！！！
  */
 public class CHECK_CAST extends Index16Instruction {
     @Override
     public void execute(Zframe frame) {
         OperandStack stack = frame.getOperandStack();
         Zobject obj = stack.popRef();
+        stack.pushRef(obj);
         //如果 obj 为 null，则可以转换为任意类型
         if (obj == null) {
             return;
