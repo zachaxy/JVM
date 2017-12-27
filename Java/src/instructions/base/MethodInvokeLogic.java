9b61b857b55fac007188960dad6116ea087140e0
zachaxy <zachaxy@163.com>
2017-12-27 20:11:45 +0800

    add the logic of invoke method

--- Java/src/instructions/base/MethodInvokeLogic.java
+++ Java/src/instructions/base/MethodInvokeLogic.java
@@ -1,8 +1,27 @@
 package instructions.base;
 
+import runtimedata.Slot;
+import runtimedata.Zframe;
+import runtimedata.Zthread;
+import runtimedata.heap.Zmethod;
+
 /**
  * @author zachaxy
  * @date 17/12/27
  */
 public class MethodInvokeLogic {
+
+    public static void invokeMethod(Zframe invokerFrame, Zmethod method) {
+        Zthread thread = invokerFrame.getThread();
+        Zframe newFrame = thread.createFrame(method);
+        thread.pushFrame(newFrame);
+
+        int argSlotCount = method.getArgSlotCount();
+        if (argSlotCount > 0) {
+            for (int i = argSlotCount - 1; i >= 0; i--) {
+                Slot slot = invokerFrame.getOperandStack().popSlot();
+                newFrame.getLocalVars().setSlot(i,slot);
+            }
+        }
+    }
 }
