af4f0578cae6e3e63f3a595aca1276601cc9a894
zachaxy <zachaxy@163.com>
2018-01-03 15:55:32 +0800

    remove native method hack

--- Java/src/instructions/base/MethodInvokeLogic.java
+++ Java/src/instructions/base/MethodInvokeLogic.java
@@ -23,14 +23,5 @@ public class MethodInvokeLogic {
                 newFrame.getLocalVars().setSlot(i,slot);
             }
         }
-
-        //TODO:hack native function,not support yet!
-        if (method.isNative()) {
-            if ("registerNatives".equals(method.getName())) {
-                thread.popFrame();
-            } /*else {
-                throw new Error(method.getName() + "is native function,not support yet!");
-            }*/
-        }
     }
 }
