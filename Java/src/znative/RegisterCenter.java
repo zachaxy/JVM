a2a0a2f648f7d4476cd41b63d42ce75b3212c5b9
zachaxy <zachaxy@163.com>
2018-01-03 16:02:49 +0800

    add native throwable

--- Java/src/znative/RegisterCenter.java
+++ Java/src/znative/RegisterCenter.java
@@ -3,7 +3,7 @@ package znative;
 import runtimedata.Zframe;
 import znative.java.lang.Nclass;
 import znative.java.lang.Nobject;
-import znative.java.lang.Zthrowable;
+import znative.java.lang.Nthrowable;
 
 import java.util.HashMap;
 
@@ -63,7 +63,7 @@ public class RegisterCenter {
 	register("java/lang/Class", "getComponentType", "()Ljava/lang/Class;", new Nclass.getComponentType())
 	register("java/lang/Class", "isAssignableFrom", "(Ljava/lang/Class;)Z", new Nclass.isAssignableFrom())
  */
-        register("java/lang/Throwable", "fillInStackTrace", "(I)Ljava/lang/Throwable;", new Zthrowable.fillInStackTrace());
+        register("java/lang/Throwable", "fillInStackTrace", "(I)Ljava/lang/Throwable;", new Nthrowable.fillInStackTrace());
 
     }
 }
